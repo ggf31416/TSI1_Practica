39c68b97c81fe2c3efde07c81cd05e86121c749a
Pablo Anzorena <anzorena.fing@gmail.com>
2016-05-04 17:28:56 -0700

    primer commit

---
+++ ClassLibrary1/DALConstruccionMongo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson.Serialization;
+
+namespace DataAccessLayer
+{
+    public class DALConstruccionMongo : IDALConstruccion
+    {
+        const string connectionstring = "mongodb://40.84.2.155";
+        protected static IMongoClient _client = new MongoClient();
+        protected static IMongoDatabase _database = _client.GetDatabase("frontoffice");
+        //var collection = _database.GetCollection<Employee>("employees");
+        public void prueba(String prueba)
+        {
+
+        }
+
+    }
+}
