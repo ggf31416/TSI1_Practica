f8749f93095c1e7a78a7faed4a4eea4bfe7216c1
Guillermo Gabrielli <ggf31416@gmail.com>
2016-05-09 17:36:06 -0300

    cambios graficos, downgrade phaser 2.27 -> 2.26 por bug

---
+++ FrontEnd/App_Start/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FrontEnd.App_Start.Startup1))]
+
+namespace FrontEnd.App_Start
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
