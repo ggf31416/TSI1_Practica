043edbc8dc8c86e7d649e1713c7a13cfcb8573b2
Guillermo Gabrielli <ggf31416@gmail.com>
2016-05-18 11:28:50 -0300

    x

--- BusinessLogicLayer/Batalla.cs
+++ BusinessLogicLayer/Batalla.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,17 @@ namespace BusinessLogicLayer
 {
     class Batalla
     {
+        private Dictionary<int, TipoUnidad> tiposUnidades = new Dictionary<int, TipoUnidad>();
+        private Dictionary<int, TipoEdificio> tiposEdificios = new Dictionary<int, TipoEdificio>();
+        private DataAccessLayer.Relacional.IDALEntidadesRO _dalRO;
         public string canalSignalR { get; set; }
         public BLTablero tablero;
+
+        void inicializar()
+        {
+            tiposUnidades = _dalRO.GetAllTipoUnidades().ToDictionary(x => x.Id);
+            tiposEdificios = _dalRO.GetAllTipoEdificios().ToDictionary(x => x.Id);
+        }
 
         private void agregarUnidades(Jugador jug)
         {
@@ -22,6 +32,7 @@ namespace BusinessLogicLayer
                 foreach(Unidad u in lst)
                 {
                     u.id = r.Next(1, Int32.MaxValue);
+                    u.jugador = jug.Identificador;
                 }
                 tablero.agregarUnidades(jug.Identificador, lst);
             }
@@ -35,9 +46,20 @@ namespace BusinessLogicLayer
             tablero.agregarEdificios(defensor.Edificios);
         }
 
-        private Unidad getUnidadPorId(int unidadId)
+        private Unidad getUnidadPorId(int tipoId)
         {
-            throw new NotImplementedException();
+            TipoUnidad tu = tiposUnidades[tipoId];
+            Unidad u = new Unidad { ataque = tu.Ataque.Value, defensa = tu.Defensa.Value, tipo_id = tu.Id, vida = tu.Vida.Value };
+            u.rango = 8; // hardcodeado;
+            return u;
+        }
+
+        private Edificio getEdificioPorId(int tipoId, int pX, int pY,string j)
+        {
+            TipoEdificio te = tiposEdificios[tipoId];
+            Edificio e = new Edificio { tipo_id = te.Id, posX = pX, posY = pY, jugador = j };
+            return e;
         }
     }
 }
+
