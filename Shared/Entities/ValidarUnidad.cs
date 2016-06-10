cef273422cf7c64ac24a2272a40a6919777c9f5b
Pablo Anzorena <anzorena.fing@gmail.com>
2016-06-10 03:32:48 -0700

    ValidarConstruccion y Unidad

---
+++ Shared/Entities/ValidarUnidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Shared.Entities
+{
+    [DataContract]
+    public class ValidarUnidad
+    {
+        [DataMember]
+        public TipoUnidad TipoUnidad { get; set; }
+        [DataMember]
+        public Dictionary<int,Recurso> recursos { get; set; }
+    }
+}
