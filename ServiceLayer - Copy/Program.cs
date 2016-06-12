811e1be0eca74d0a50d94b38b4e983d89b01d037
Manuel <manu.alzugaray@gmail.com>
2016-06-12 16:06:07 -0300

    agrego carpetas copy

---
+++ ServiceLayer - Copy/Program.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    class Program
+    {
+        public static IBLTablero blHandler;
+
+        static void Main(string[] args)
+        {
+            SetupDependencies();
+            SetupService();
+        }
+
+        private static void SetupDependencies()
+        {
+            //blHandler = new BLTablero(new DataAccessLayer.DALTablero());
+        }
+
+        private static void SetupService()
+        {
+            Uri baseAddress = new Uri("http://localhost:8837/tsi/");
+            ServiceHost selfHost = new ServiceHost(typeof(ServiceInteraccion), baseAddress);
+            try
+            {
+                selfHost.AddServiceEndpoint(
+                        typeof(IServiceInteraccion),
+                        new BasicHttpBinding(),
+                        "ServiceInteraccion");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                selfHost.Open();
+                Console.WriteLine("ServiceLayer2");
+                Console.ReadLine();
+                selfHost.Close();
+            }catch (CommunicationException ce)
+            {
+                Console.Write("An exception ocurred: " +  ce.ToString());
+                Console.ReadLine();
+                selfHost.Abort();
+            }
+        }
+    }
+}
